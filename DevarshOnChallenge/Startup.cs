73663b0e473911b8782ae85a89f1bb93510ecf21
Devarsh shukla <Devarsh14@gmail.com>
2019-03-20 12:15:40 +1000

    Added stylecop analyzer for syntax highlighting and code consitency.

--- DevarshOnChallenge/Startup.cs
+++ DevarshOnChallenge/Startup.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using NJsonSchema;
-using NSwag.AspNetCore;
-
-namespace DevarshOnChallenge
+﻿namespace DevarshOnChallenge
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using NJsonSchema;
+    using NSwag.AspNetCore;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -27,9 +27,8 @@ namespace DevarshOnChallenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+
             // Register the Swagger services
             services.AddSwaggerDocument();
         }
@@ -49,6 +48,7 @@ namespace DevarshOnChallenge
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
             // Register the Swagger generator and the Swagger UI middlewares
             app.UseSwagger();
             app.UseSwaggerUi3();
--- ReadifyChallengeTest/UnitTest1.cs
+++ ReadifyChallengeTest/UnitTest1.cs
@@ -1,8 +1,12 @@
-using System;
-using Xunit;
+// <copyright file="UnitTest1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace ReadifyChallengeTest
 {
+    using System;
+    using Xunit;
+
     public class UnitTest1
     {
         [Fact]
